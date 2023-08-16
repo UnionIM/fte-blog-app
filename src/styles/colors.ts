5a2721e8ca9c349653992087805aaae4c1fa7685
Danylo Honcharov <Dan.rowv@gmail.com>
2023-08-16 16:45:14 +0300

    Colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,19 @@
+export enum Colors {
+  blue = "#1D4ED8",
+  lightBlue = "#3B82F6",
+  green = "#15803D",
+  red = "#B91C1C",
+  yellow = "#B45309",
+  black = "#374151",
+}
+
+export enum DarkColors {
+  blue = "#1E40AF",
+  lightBlue = "#3B82F6",
+  green = "#166534",
+  red = "#991B1B",
+  yellow = "#854D0E",
+  black = "#1F2937",
+}
+
+export type TColor = "blue" | "green" | "red" | "yellow" | "black";
