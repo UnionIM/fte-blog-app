71c0a3d35b06b2b499b7a02fc156d3bbbd71568d
Danylo Honcharov <Dan.rowv@gmail.com>
2023-08-16 16:39:14 +0300

    Fonts, images

---
+++ src/styles/fonts/h2.ts
@@ -0,0 +1,7 @@
+import { Colors } from "../colors";
+
+export const h2 = {
+  fontWeight: 500,
+  fontSize: "36px",
+  color: Colors.blue,
+};
---
+++ src/styles/fonts/logo.ts
@@ -0,0 +1,7 @@
+import { Colors } from "../colors";
+
+export const logo = {
+  fontWeight: 300,
+  fontSize: "16px",
+  color: Colors.blue,
+};
