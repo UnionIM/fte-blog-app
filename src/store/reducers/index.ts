6763d857bed62788edc0be71b0a6bfab67a914e3
Danylo Honcharov <Dan.rowv@gmail.com>
2023-08-16 16:30:35 +0300

    postApi

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { userSlice } from "./UserSlicer";
 import { setupStore } from "../store";
+import { postApi } from "../../service/PostService";
 
 export const rootReducer = combineReducers({
   user: userSlice.reducer,
+  [postApi.reducerPath]: postApi.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
