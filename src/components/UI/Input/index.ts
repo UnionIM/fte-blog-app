200502fb9df755df608c8fba67d0e7092dd35c17
Danylo Honcharov <Dan.rowv@gmail.com>
2023-08-22 12:59:52 +0300

    feat: add more props, focus effect, base styles

--- src/components/UI/Input/index.ts
+++ src/components/UI/Input/index.ts
@@ -1,11 +1,24 @@
 import styled from "@emotion/styled";
-import { IInput } from "./Input";
-import { Colors } from "../../../styles/colors";
+import { Colors, TColor } from "../../../styles/colors";
+import { baseStyles } from "../../../styles/baseStyles";
+import { IBaseStyles } from "../../../models/IBaseStyles";
 
-export const StyledInput = styled.input`
-  border-color: ${({ borderColor }: IInput) => Colors[borderColor || "blue"]};
-  border-style: solid;
-  border-width: 1px;
+export const StyledInput = styled.input<{
+  borderColor?: TColor | "gray";
+  sx?: Partial<IBaseStyles>;
+}>`
+  font-size: 14px;
   border-radius: 8px;
   padding: 9px;
+  outline: none;
+  &:focus {
+    border: ${({ borderColor }) => Colors[borderColor || "blue"]} 1px solid !important;
+  }
+  &:not(placeholder-shown) {
+    border: ${({ borderColor }) => Colors[borderColor || "blue"]} 1px solid;
+  }
+  &:placeholder-shown {
+    border: ${Colors.gray} 1px solid;
+  }
+  ${({ sx }) => baseStyles(sx)};
 `;
