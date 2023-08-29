9974939ee9496020a1c096bb79329c6314d2edca
Danylo Honcharov <Dan.rowv@gmail.com>
2023-08-29 11:47:33 +0300

    Star component

---
+++ src/styles/components/Star/index.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled/dist/emotion-styled.cjs";
+
+export const FilledStar = styled.span`
+  color: yellow;
+`;
+
+export const EmptyStar = styled.span`
+  color: gray;
+`;
